9e3f604eee8caa5a2e043c15b6898c7e92630bd3
ptr-21 <157449192+ptr-21@users.noreply.github.com>
2024-12-07 18:01:40 +0700

    Update get data eyyo2.php

--- eyyo2.php
+++ eyyo2.php
@@ -1,5 +1,7 @@
 <?php
 session_start(); // Mulai sesi
+$nama_pengguna = isset($_SESSION['nama']) ? htmlspecialchars($_SESSION['nama']) : '';
+$nomor_telpon = isset($_SESSION['nomor_telpon']) ? htmlspecialchars($_SESSION['nomor_telpon']) : '';
 ?>
 
 <!DOCTYPE html>
@@ -155,12 +157,12 @@ session_start(); // Mulai sesi
                     <div class="col-md-6">
                         <div class="mb-3">
                             <label for="namaPengadu" class="form-label">Nama Pengadu</label>
-                            <textarea class="form-control" id="namaPengadu" name="namaPengadu" rows="3" placeholder="Masukkan nama anda" required></textarea>
+                            <textarea class="form-control" id="namaPengadu" name="namaPengadu" rows="3" placeholder="Masukkan nama anda" required readonly><?php echo $nama_pengguna; ?></textarea>
                         </div>
 
-                        <div class="mb-3">
+                        <<div class="mb-3">
                             <label for="noTeleponPengadu" class="form-label">No Telepon Pengadu</label>
-                            <textarea class="form-control" id="noTeleponPengadu" name="noTeleponPengadu" rows="3" placeholder="Masukkan nomor anda" required></textarea>
+                            <textarea class="form-control" id="noTeleponPengadu" name="noTeleponPengadu" rows="3" placeholder="Masukkan nomor anda" required readonly><?php echo $nomor_telpon; ?></textarea>
                         </div>
 
                         <div class="mb-3">

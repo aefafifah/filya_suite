6346cad3723f2ffc6c8a74bf57930023de659186
Stellar Mohammad Tri Bagus <115438548+MohammadTriBagus@users.noreply.github.com>
2024-12-24 12:38:55 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset(); // Menghapus semua variabel sesi
 session_destroy(); // Menghancurkan sesi
-header("Location: login.php"); // Alihkan kembali ke halaman login
+header("Location: index.php"); // Alihkan kembali ke halaman login
 exit();

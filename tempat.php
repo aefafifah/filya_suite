25718e5b6c8c2f3952ac4a3bd07c56879a0d8084
Stellar Mohammad Tri Bagus <115438548+MohammadTriBagus@users.noreply.github.com>
2024-12-24 12:42:05 +0700

    Update tempat.php

--- tempat.php
+++ tempat.php
@@ -1,224 +1,389 @@
 <?php
-
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "filya_suite";
-
 session_start();
 
-$data = mysqli_connect($host, $user, $password, $db);
+// Koneksi database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "filya_suite";
+$data = mysqli_connect($servername, $username, $password, $dbname);
 
-if ($data === false) {
-    die("connection error");
+if (!$data) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 
 
-if(isset($_POST['submit'])){
-    $name = mysqli_real_escape_string($data, $_POST['nama']);
-    $email = mysqli_real_escape_string($data, $_POST['email']);
-    $pass = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash password
-    $cpass = password_hash($_POST['cpassword'], PASSWORD_DEFAULT); // Ini tidak perlu, lihat penjelasan di bawah
-    $phone = mysqli_real_escape_string($data, $_POST['nomor_telpon']);
-    $address = mysqli_real_escape_string($data, $_POST['alamat']);
-    
-    $user_type = 'user';
-    $select = "SELECT * FROM users WHERE email = '$email'";
+if (!isset($_SESSION['nomor_telpon']) && !isset($_SESSION['email'])) {
+    // For guests
+    $nama_pengguna = "Tamu";
+    $usertype = "guest";
+    $nomor_telpon = "";
+} else {
 
-    $result = mysqli_query($data, query: $select);
+    $nama_pengguna = $_SESSION['nama'] ?? '';
+    $nomor_telpon = $_SESSION['nomor_telpon'] ?? '';
+    $usertype = $_SESSION['usertype'];
+}
 
-    if(mysqli_num_rows($result) > 0){
-        $error[] = 'User already exists!';
-    } else {
-        // Cek kecocokan password
-        if ($_POST['password'] != $_POST['cpassword']) {
-            $error[] = 'Password not matched!';
-        } else {
-            // Hanya simpan hash password yang benar
-            $insert = "INSERT INTO users(nama, email, password, usertype, nomor_telpon, alamat) VALUES('$name','$email','$pass','$user_type','$phone','$address')";
-            mysqli_query($data, $insert);
-            $_SESSION['nama'] = $name;
-            $_SESSION['nomor_telpon'] = $phone;
-            $_SESSION['user_id'] = mysqli_insert_id($data); // Mengambil ID pengguna terakhir yang dimasukkan
-            $_SESSION['usertype'] = $user_type; // Atur tipe pengguna
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-            header('location:userhome.php');
+    $nama_pengguna = $_SESSION['nama'] ?? 'Guest';
 
 
+    $nomor_telpon = $_SESSION['nomor_telpon'] ?? ($_POST['noTelepon'] ?? 'Tidak Tersedia');
+    $tanggalMenginap = htmlspecialchars(trim($_POST['tanggalMenginap']));
+    $report_date = htmlspecialchars(trim($_POST['report_date']));
+    $description = htmlspecialchars(trim($_POST['description']));
+    $damage_location = htmlspecialchars(trim($_POST['damage_location']));
+    $category = htmlspecialchars(trim($_POST['category']));
+
+
+    $tanggalMenginapDate = new DateTime($tanggalMenginap);
+    $reportDate = new DateTime($report_date);
+    $currentDate = new DateTime();
+
+    if ($reportDate < $tanggalMenginapDate) {
+        die("Tanggal laporan tidak boleh lebih awal dari tanggal menginap.");
+    }
+
+    if ($tanggalMenginapDate > $currentDate) {
+        die("Tanggal menginap tidak boleh lebih dari hari ini.");
+    }
+
+
+    $uploadOk = 1;
+    $target_dir = "uploads/";
+    $image = $_FILES['image-upload'];
+    $target_file = $target_dir . uniqid() . basename($image["name"]);
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+    if ($image) {
+        $check = getimagesize($image["tmp_name"]);
+        if ($check === false) {
+            die("File ini bukan gambar.");
+        }
+        if ($image["size"] > 2000000) {
+            die("Maaf, file Anda terlalu besar.");
+        }
+        if (!in_array($imageFileType, ['jpg', 'jpeg', 'png', 'gif'])) {
+            die("Hanya file JPG, JPEG, PNG & GIF yang diperbolehkan.");
+        }
+        if (!move_uploaded_file($image["tmp_name"], $target_file)) {
+            die("Terjadi kesalahan saat mengunggah file.");
         }
     }
+
+    // Tambahkan data laporan ke tabel tempat
+    $stmt = $data->prepare("INSERT INTO tempat (nama_pengadu, no_telepon_pengadu, tanggal_menginap, nomor_kamar, jenis_masalah, deskripsi_masalah, file_bukti, waktu_pengaduan) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssss", $nama_pengguna, $nomor_telpon, $tanggalMenginap, $damage_location, $category, $description, $target_file, $report_date);
+
+    if ($stmt->execute()) {
+        header("Location: tempat.php?status=success");
+        exit();
+    } else {
+        error_log("Error saat menyimpan laporan: " . $stmt->error);
+        header("Location: tempat.php?status=error");
+        exit();
+    }
+
 }
-
-
-
+$data->close();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Register Form</title>
-
-   <!-- Bootstrap CSS -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-   <style>
-      body {
-         background-image: url('assets/login-register.jpg');
-         background-size: cover;
-         background-repeat: no-repeat;
-         height: 100vh;
-         display: flex;
-         align-items: center;
-         justify-content: center;
-         margin: 0;
-      }
-
-      .card {
-         background-color: rgba(255, 255, 255, 0.9);
-         border-radius: 8px;
-         box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-         padding: 20px;
-         width: 550px;
-         height:800px;
-         position: absolute;
-         top: 90px; 
-         left: 70%; 
-         transform: translateX(-20%);
-      }
-
-      .card-header {
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan Tempat</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.8/dist/sweetalert2.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.8/dist/sweetalert2.all.min.js"></script>
+    <style>
+        body {
+            width: 100vw;
+            height: 100vh;
+            margin: 0;
+            font-family: 'Outfit', sans-serif;
+            color: #FDE49E;
             display: flex;
-            justify-content: space-around;
-            background-color: transparent;
-            padding: 0;
-            border-radius: 5px 5px 0 0;
-            overflow: hidden;
+            justify-content: center;
+            align-items: center;
         }
-        .card-header div {
-            flex: 1;
-            padding: 10px 0;
+
+        .blur-bg {
+            background-image: url('bglogin-register.jpg');
+            background-size: cover;
+            background-position: center;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100vh;
+            z-index: -1;
+        }
+
+        .blur-bg::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(28, 24, 24, 0.7);
+            z-index: 1;
+        }
+
+        .form-container {
+            padding: 40px;
+            border-radius: 10px;
+            position: relative;
+            z-index: 2;
+            width: 95%;
+            max-width: 850px;
+        }
+
+        h1 {
+            color: #DD761C;
+            font-size: 64px;
             text-align: center;
-            font-weight: bold;
-            color: #ffa500;
-            background-color: #ffedcc;
+            margin-top: 40px;
+            margin-bottom: 20px;
+        }
+
+        label {
+            font-size: 14px;
+            display: block;
+            margin: 10px 0 5px;
+        }
+
+        .form-row {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 20px;
+        }
+
+        .form-row div {
+            flex: 1;
+            margin-right: 20px;
+        }
+
+        .form-row div:last-child {
+            margin-right: 0;
+        }
+
+        input[type="text"],
+        input[type="tel"],
+        input[type="date"],
+        textarea,
+        select {
+            width: 100%;
+            height: 55px;
+            padding: 12px;
+            font-size: 16px;
+            border: none;
+            border-radius: 5px;
+            box-sizing: border-box;
+        }
+
+        textarea {
+            height: 55px;
+            resize: none;
+        }
+
+        #image-upload {
+            height: 55px;
+            padding: 12px;
+            font-size: 16px;
+            border: none;
+            border-radius: 5px;
+            box-sizing: border-box;
+        }
+
+        button {
+            height: 50px;
+            border: none;
+            border-radius: 5px;
+            font-size: 18px;
             cursor: pointer;
         }
-        .card-header .active {
-            background-color: #ff8c00;
+
+        .button-container {
+            display: flex;
+            justify-content: center;
+            gap: 20px;
+            margin-top: 20px;
+        }
+
+        .button-container button {
+            width: 130px;
+        }
+
+        .button-container .back-button {
+            background-color: white;
+            color: #DD761C;
+        }
+
+        .button-container .submit-button {
+            background-color: #DD761C;
             color: white;
         }
-      h2, h3 {
-         color: #ff8c00;;
-         font-family: 'Arial', sans-serif;
-         font-weight: bold;
-      }
 
-
-      h3 {
-         font-size: 24px;
-      }
-
-      .form-label {
-         font-weight: bold;
-         color: #333;
-      }
-
-      .form-control {
-         border-radius: 5px;
-         border: 1px solid #ced4da;
-         transition: border-color 0.2s;
-      }
-
-      .form-control:focus {
-         border-color: #007bff;
-         box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
-      }
-
-      .btn-primary {
-            background-color: #ff8c00;
-            border: none;
-            width: 100%;
-            padding: 10px;
-            font-size: 16px;
+        input[type="text"],
+        input[type="tel"],
+        input[type="date"],
+        textarea,
+        select {
+            width: calc(100% + 20px);
             margin-bottom: 10px;
         }
 
-      .btn-primary:hover {
-         
-            background-color: #e67e22;
-     
-        
-      }
+        #image-upload {
+            width: calc(100% + 20px);
+            margin-bottom: 10px;
+        }
 
-      .text-center a {
-         color: #007bff;
-         text-decoration: none;
-      }
+        .form-row {
+            gap: 20px;
+        }
+    </style>
 
-      .text-center a:hover {
-         text-decoration: underline;
-      }
-
-      .alert {
-         margin-top: 10px;
-      }
-
-      p {
-         color: #333;
-         font-size: 14px;
-      }
-   </style>
 </head>
+
 <body>
-<div class="card">
-    <div class="card-header">
-        <div onclick="window.location.href='index.php'">LOGIN</div>
-        <div class="active">REGISTER</div>
-    </div>
-    <div class="card-body">
-        <h2 class="text-center mt-3">Pantau Laporanmu?</h2>
-        <h3 class="text-center mt-2">FilyaSuite Siap Membantu</h3>
-        <form action="" method="post">
-            <?php if (isset($error)) {
-                foreach ($error as $errors) {
-                    echo '<div class="alert alert-danger">'.$errors.'</div>';
-                }
-            } ?>
-            <div class="mb-2">
-                <label for="nama" class="form-label">Nama</label>
-                <input type="text" class="form-control" name="nama" id="nama" required placeholder="Masukkan Nama Anda">
+    <div class="blur-bg"></div>
+
+    <div class="form-container">
+        <h1>LAPORAN TEMPAT</h1>
+        <?php
+        $status = filter_input(INPUT_GET, 'status', FILTER_SANITIZE_STRING);
+        if ($status === 'success') {
+            echo "<script>
+        Swal.fire({
+            icon: 'success',
+            title: 'Pengaduan berhasil dikirim!',
+            text: 'Terima kasih atas laporan Anda.',
+            showConfirmButton: true
+        }).then(function() {
+            window.location.href = 'userhome.php';
+        });
+    </script>";
+        } elseif ($status === 'error') {
+            echo "<script>
+        Swal.fire({
+            icon: 'error',
+            title: 'Terjadi kesalahan!',
+            text: 'Silakan coba lagi.',
+            showConfirmButton: true
+        });
+    </script>";
+        }
+        ?>
+
+        <form action="" method="POST" enctype="multipart/form-data">
+            <div class="form-row">
+                <div>
+                    <label for="name">Nama Pengadu:</label>
+                    <textarea id="name" name="name" required><?php echo $nama_pengguna; ?></textarea>
+                </div>
+                <div>
+                    <label for="report_date">Tanggal Melaporkan:</label>
+                    <input type="date" id="report_date" name="report_date" required>
+                </div>
             </div>
-            <div class="mb-2">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" name="email" id="email" required placeholder="Masukkan Email Anda">
+
+            <!-- Baris Nomor Telepon dan Tanggal Menginap -->
+            <div class="form-row">
+                <div>
+                    <label for="phone">Nomor Telpon:</label>
+                    <textarea id="phone" name="phone" readonly required><?php echo htmlspecialchars($nomor_telpon, ENT_QUOTES, 'UTF-8'); ?></textarea>
+                </div>
+                <div>
+                    <label for="tanggalMenginap">Tanggal Menginap:</label>
+                    <input type="date" id="tanggalMenginap" name="tanggalMenginap" required>
+                </div>
             </div>
-            <div class="mb-2">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" name="password" id="password" required placeholder="Masukkan Password Anda">
+
+            <div class="form-row">
+                <div>
+                    <label for="description">Deskripsi Masalah:</label>
+                    <textarea id="description" name="description" rows="4" required></textarea>
+                </div>
+                <div>
+                    <label for="category">Kategori Masalah:</label>
+                    <select id="category" name="category" required>
+                        <option value="Kerusakan Bangunan">Kerusakan Bangunan</option>
+                        <option value="Kebersihan Villa">Kebersihan Villa</option>
+                        <option value="Perawatan yang Kurang">Perawatan yang Kurang</option>
+                        <option value="Suasana Villa Merasa Tidak Aman atau Lainnya">Suasana Villa Merasa Tidak Aman
+                            atau Lainnya</option>
+                        <option value="Perbedaan Kondisi Properti dengan Foto Properti di Iklan">Perbedaan Kondisi
+                            Properti dengan Foto Properti di Iklan</option>
+                        <option value="Masalah Aksesibilitas">Masalah Aksesibilitas</option>
+                        <option value="Kenyamanan pada Waktu Penginapan">Kenyamanan pada Waktu Penginapan</option>
+                        <option value="Lainnya">Lainnya</option>
+                    </select>
+                </div>
             </div>
-            <div class="mb-2">
-                <label for="cpassword" class="form-label">Konfirmasi Password</label>
-                <input type="password" class="form-control" name="cpassword" id="cpassword" required placeholder="Konfirmasi Password Anda">
+
+            <div class="form-row">
+                <div>
+                    <label for="image-upload">Bukti Masalah:</label>
+                    <input type="file" class="form-control" id="image-upload" name="image-upload" accept="image/*"
+                        required>
+                </div>
+                <div>
+                    <label for="damage_location">Nomor Kamar:</label>
+                    <select id="damage_location" name="damage_location" required>
+                        <option value="Dreamy">Dreamy</option>
+                        <option value="Fancy">Fancy</option>
+                        <option value="Charming">Charming</option>
+                    </select>
+                </div>
             </div>
-            <div class="mb-2">
-                <label for="nomor_telpon" class="form-label">Nomor Telpon</label>
-                <input type="text" class="form-control" name="nomor_telpon" id="nomor_telpon" required placeholder="Masukkan Nomor Telpon Anda">
+
+            <div class="button-container">
+                <button type="button" onclick="window.location.href='userhome.php';">Kembali</button>
+                <button type="submit" class="submit-button">Submit</button>
             </div>
-            <div class="mb-2">
-                <label for="alamat" class="form-label">Alamat</label>
-                <input type="text" class="form-control" name="alamat" id="alamat" required placeholder="Masukkan Alamat Anda">
-            </div>
-            <div class="d-grid mt-4">
-                <button type="submit" class="btn btn-primary" name="submit">Registrasi Sekarang</button>
-            </div>
-            <p class="text-center mt-3">Apakah Anda Sudah Mempunyai Akun? <a href="index.php">Login Sekarang</a></p>
         </form>
     </div>
-</div>
-   <!-- Bootstrap JS -->
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
 
+<script>
+    const checkInInput = document.getElementById('tanggalMenginap');
+    const reportInput = document.getElementById('report_date');
+
+
+    function validateDates() {
+
+        let tanggalMenginap = new Date(checkInInput.value);
+        let report_date = new Date(reportInput.value);
+        let currentDate = new Date();
+
+        if (report_date && tanggalMenginap && report_date < tanggalMenginap) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Tanggal Laporan Tidak Valid',
+                text: 'Tanggal laporan tidak boleh lebih awal dari tanggal menginap!',
+            });
+            return;
+        }
+
+        // Validasi jika tanggal menginap lebih dari tanggal saat ini
+        if (tanggalMenginap && tanggalMenginap > currentDate) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Tanggal Menginap Tidak Valid',
+                text: 'Tanggal menginap tidak boleh lebih dari tanggal saat ini!',
+            });
+            return;
+        }
+    }
+
+    checkInInput.addEventListener('input', validateDates);
+    reportInput.addEventListener('input', validateDates);
+</script>
+
+</html>
